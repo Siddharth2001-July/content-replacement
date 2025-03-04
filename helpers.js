7831318d69dda0d4c6400de54c8c54fd34169c2d
Siddharth Kamboj <71596674+Siddharth2001-July@users.noreply.github.com>
2025-03-04 10:30:39 +0530

    using overlays

--- helpers.js
+++ helpers.js
@@ -1,4 +1,12 @@
 let renderedForPageIndex = [];
+
+// Function to remove "px" and convert pixels to PDF points
+function pixelsToPoints(pixelValue, dpi = 96) {
+  const numericValue = parseFloat(pixelValue.replace("px", ""));
+  // return (numericValue / dpi) * 72;
+  return numericValue
+}
+
 export async function createRect(
   PSPDFKit,
   instance,
@@ -8,11 +16,17 @@ export async function createRect(
 ) {
   const annotation = new PSPDFKit.Annotations.RectangleAnnotation({
     pageIndex,
+    // boundingBox: new PSPDFKit.Geometry.Rect({
+    //   left: element.boundingBox.left-1,
+    //   top: element.boundingBox.top-1,
+    //   width: element.boundingBox.width+2,
+    //   height: element.boundingBox.height+2,
+    // }),
     boundingBox: new PSPDFKit.Geometry.Rect({
-      left: element.boundingBox.left-1,
-      top: element.boundingBox.top-1,
-      width: element.boundingBox.width+2,
-      height: element.boundingBox.height+2,
+      left: pixelsToPoints(element.left),
+      top: pixelsToPoints(element.top),
+      width: pixelsToPoints(element.width),
+      height: pixelsToPoints(element.height),
     }),
     strokeWidth,
   });
@@ -22,7 +36,9 @@ export async function createRect(
 export async function mapLinesForEachPage(PSPDFKit, instance) {
   const pages = instance.totalPageCount;
   for (let index = 0; index < pages; index++) {
-    const lines = await instance.textLinesForPageIndex(index);
+    // const lines = await instance.textLinesForPageIndex(index);
+    const lines = window.textBlocks;
+
     lines.forEach(async (element) => {
       await createRect(PSPDFKit, instance, element, index);
     });
@@ -31,7 +47,8 @@ export async function mapLinesForEachPage(PSPDFKit, instance) {
 
 export async function createRectsWhenScrolled(PSPDFKit, instance, index) {
   if (!renderedForPageIndex.includes(index)) {
-    const lines = await instance.textLinesForPageIndex(index);
+    // const lines = await instance.textLinesForPageIndex(index);
+    const lines = window.textBlocks[index];
     await lines.forEach(async (element) => {
       await createRect(PSPDFKit, instance, element, index, 1);
     });
--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 import {
-  createRect,
   createRectsWhenScrolled,
-  mapLinesForEachPage,
 } from "./helpers.js";
 
 const PSPDFKit = window.PSPDFKit;
@@ -14,17 +12,64 @@ async function runner(docPath) {
     baseUrl,
     container: "#pspdfkit",
     document: docPath,
-    toolbarItems: [...PSPDFKit.defaultToolbarItems, { type: "comment" }],
+    toolbarItems: [...PSPDFKit.defaultToolbarItems, { type: "comment" }, { type: "content-editor" }],
   })
     .then(async (instance) => {
+      window.instance = instance
+      const response = await fetch("mapping.json");
+      const mappingData = await response.json();
+      if (docPath.includes("2")) {
+        mappingData.TOFU_2.forEach(element => {
+          const overlayDiv = document.createElement("div");
+          overlayDiv.style.position = "absolute";
+          overlayDiv.style.border = "2px solid blue";        // initial border color
+          overlayDiv.style.backgroundColor = "transparent";  // transparent background
+          overlayDiv.style.width = `${element.boundingBox.width}px`;
+          overlayDiv.style.height = `${element.boundingBox.height}px`;
+          overlayDiv.addEventListener("click", () => {
+            overlayDiv.style.borderColor = overlayDiv.style.borderColor === "blue" ? "red" : "blue";
+          });
+
+          const item = new PSPDFKit.CustomOverlayItem({
+            id: `overlay-${element.id}`,
+            node: overlayDiv,
+            pageIndex: element.pageIndex,
+            position: new PSPDFKit.Geometry.Point({ x: element.boundingBox.left, y: element.boundingBox.top })
+          });
+          instance.setCustomOverlayItem(item);
+        });
+      }
       //intializing for first page when loaded
-      createRectsWhenScrolled(PSPDFKit, instance, 0);
       // lazy creating rects when user scrolls to them
-      instance.addEventListener(
-        "viewState.currentPageIndex.change",
-        (newPageIndex) =>
-          createRectsWhenScrolled(PSPDFKit, instance, newPageIndex)
-      );
+      // instance.setViewState(viewState =>
+      //   viewState.set(
+      //     "interactionMode",
+      //     PSPDFKit.InteractionMode.CONTENT_EDITOR
+      //   )
+      // );
+      // setTimeout(() => {
+      //   createRectsWhenScrolled(PSPDFKit, instance, 0);
+      // }, 2000);
+      // instance.addEventListener(
+      //   "viewState.currentPageIndex.change",
+      //   (newPageIndex) =>
+      //     createRectsWhenScrolled(PSPDFKit, instance, newPageIndex)
+      // );
+      // instance.addEventListener("annotations.press", (event) => {
+      //   if (event.annotation instanceof PSPDFKit.Annotations.RectangleAnnotation) {
+
+      //     let annot = event.annotation
+      //     if (annot.strokeColor == PSPDFKit.Color.BLUE) {
+      //       console.log("Rect clicked BLUE");
+      //       annot = annot.set("strokeColor", PSPDFKit.Color.GREY)
+      //     }
+      //     else {
+      //       console.log("Rect clicked ELSE");
+      //       annot = annot.set("strokeColor", PSPDFKit.Color.BLUE)
+      //     }
+      //     instance.update([annot])
+      //   }
+      // })
     })
     .catch((error) => {
       console.error(error.message);
